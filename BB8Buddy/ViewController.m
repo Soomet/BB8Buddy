eee2bf7297aaa170433002554dc9d9d8cd77de5c
Stephen Phillips <stephen.phillips@affectiva.com>
2017-03-07 03:32:04 -0500

    [SDK-1120] Update to new launch screen and logo; get app to build for simulator

--- BB8Buddy/ViewController.m
+++ BB8Buddy/ViewController.m
@@ -5,8 +5,10 @@
 
 @interface ViewController() <AFDXDetectorDelegate>
 
+#if !TARGET_OS_SIMULATOR
 @property (strong, nonatomic) RKConvenienceRobot* robot;
 @property (strong, nonatomic) RUICalibrateGestureHandler *calibrateHandler;
+#endif
 @property (strong, nonatomic) AFDXDetector *detector;
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 @property (strong, nonatomic) IBOutlet UIImageView *forceView;
@@ -20,13 +22,21 @@
 
 - (void)detector:(AFDXDetector *)detector didStartDetectingFace:(AFDXFace *)face;
 {
+#if !TARGET_OS_SIMULATOR
     [self.robot setLEDWithRed:0.0 green:1.0 blue:0.0];
+#endif
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+#if !TARGET_OS_SIMULATOR
         [self.robot setLEDWithRed:0.0 green:0.0 blue:0.0];
+#endif
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+#if !TARGET_OS_SIMULATOR
             [self.robot setLEDWithRed:0.0 green:1.0 blue:0.0];
+#endif
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+#if !TARGET_OS_SIMULATOR
                 [self.robot setLEDWithRed:0.0 green:0.0 blue:0.0];
+#endif
                 self.drawFacePoints = TRUE;
             });
         });
@@ -36,14 +46,22 @@
 - (void)detector:(AFDXDetector *)detector didStopDetectingFace:(AFDXFace *)face;
 {
     [self.forceView setImage:nil];
+#if !TARGET_OS_SIMULATOR
     [self.robot sendCommand:[RKRollCommand commandWithStopAtHeading:0]];
     [self.robot setLEDWithRed:1.0 green:0.0 blue:0.0];
+#endif
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+#if !TARGET_OS_SIMULATOR
         [self.robot setLEDWithRed:0.0 green:0.0 blue:0.0];
+#endif
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+#if !TARGET_OS_SIMULATOR
             [self.robot setLEDWithRed:1.0 green:0.0 blue:0.0];
+#endif
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+#if !TARGET_OS_SIMULATOR
                 [self.robot setLEDWithRed:0.0 green:0.0 blue:0.0];
+#endif
                 self.drawFacePoints = FALSE;
             });
         });
@@ -52,11 +70,13 @@
 
 - (void)detector:(AFDXDetector *)detector hasResults:(NSMutableDictionary *)faces forImage:(UIImage *)image atTime:(NSTimeInterval)time;
 {
-    if (nil == faces) {
+    if (nil == faces)
+    {
         // put up frame in view
         UIImage *newImage = image;
         
-        if (self.drawFacePoints) {
+        if (self.drawFacePoints)
+        {
             newImage = [AFDXDetector imageByDrawingPoints:self.facePoints
                                             andRectangles:nil
                                                 andImages:nil
@@ -68,7 +88,8 @@
         }
 
         // flip image if the front camera is being used so that the perspective is mirrored.
-        if (self.cameraToUse == AFDX_CAMERA_FRONT) {
+        if (self.cameraToUse == AFDX_CAMERA_FRONT)
+        {
             UIImage *flippedImage = [UIImage imageWithCGImage:newImage.CGImage
                                                         scale:newImage.scale
                                                   orientation:UIImageOrientationUpMirrored];
@@ -88,25 +109,32 @@
         if (face.expressions.browFurrow > 20)
         {
             // negative
+#if !TARGET_OS_SIMULATOR
             [self.robot sendCommand:[RKRollCommand commandWithHeading:180 andVelocity:face.expressions.browFurrow / 100.0]];
+#endif
             [self.forceView setImage:[UIImage imageNamed:@"darkside.png"]];
         }
         else if (face.expressions.browRaise > 20)
         {
             // Positive
+#if !TARGET_OS_SIMULATOR
             [self.robot sendCommand:[RKRollCommand commandWithHeading:0 andVelocity:face.expressions.browRaise / 100.0]];
+#endif
             [self.forceView setImage:[UIImage imageNamed:@"lightside.png"]];
         }
         else
         {
             // Neutral
+#if !TARGET_OS_SIMULATOR
             [self.robot sendCommand:[RKRollCommand commandWithStopAtHeading:0]];
+#endif
             [self.forceView setImage:nil];
         }
     }
 }
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
 
     [[NSNotificationCenter defaultCenter] addObserver:self
@@ -121,37 +149,54 @@
 
     self.cameraToUse = AFDX_CAMERA_FRONT;
 
+#if !TARGET_OS_SIMULATOR
     self.calibrateHandler = [[RUICalibrateGestureHandler alloc] initWithView:self.view];
     
     // hook up for robot state changes
     [[RKRobotDiscoveryAgent sharedAgent] addNotificationObserver:self selector:@selector(handleRobotStateChangeNotification:)];
-    
+#endif
+
     self.detector = [[AFDXDetector alloc] initWithDelegate:self usingCamera:self.cameraToUse maximumFaces:1 faceMode:LARGE_FACES];
     self.detector.browRaise = YES;
     self.detector.browFurrow = YES;
     [self.detector start];
+
+#if TARGET_OS_SIMULATOR
+    [self.forceView setImage:[UIImage imageNamed:@"lightside.png"]];
+#endif
 }
 
 
-- (void)appDidBecomeActive:(NSNotification*)notification {
+- (void)appDidBecomeActive:(NSNotification*)notification
+{
+#if !TARGET_OS_SIMULATOR
     [RKRobotDiscoveryAgent startDiscovery];
+#endif
 }
 
 
-- (void)appWillResignActive:(NSNotification*)notification {
+- (void)appWillResignActive:(NSNotification*)notification
+{
+#if !TARGET_OS_SIMULATOR
     [RKRobotDiscoveryAgent stopDiscovery];
     [RKRobotDiscoveryAgent disconnectAll];
+#endif
 }
 
-- (void)handleRobotStateChangeNotification:(RKRobotChangedStateNotification*)n {
-    switch(n.type) {
+- (void)handleRobotStateChangeNotification:(RKRobotChangedStateNotification*)n
+{
+#if !TARGET_OS_SIMULATOR
+    switch(n.type)
+    {
         case RKRobotConnecting:
             [self handleConnecting];
             break;
-        case RKRobotOnline: {
+        case RKRobotOnline:
+        {
             // Do not allow the robot to connect if the application is not running
             RKConvenienceRobot *convenience = [RKConvenienceRobot convenienceWithRobot:n.robot];
-            if ([[UIApplication sharedApplication] applicationState] != UIApplicationStateActive) {
+            if ([[UIApplication sharedApplication] applicationState] != UIApplicationStateActive)
+            {
                 [convenience disconnect];
                 return;
             }
@@ -167,38 +212,61 @@
         default:
             break;
     }
+#endif
 }
 
-- (void)handleConnecting {
+- (void)handleConnecting
+{
     // Handle when a robot is connecting here
 }
 
-- (void)handleConnected {
+- (void)handleConnected
+{
+#if !TARGET_OS_SIMULATOR
     [_calibrateHandler setRobot:_robot.robot];
+#endif
 }
 
-- (void)handleDisconnected {
+- (void)handleDisconnected
+{
+#if !TARGET_OS_SIMULATOR
     [_calibrateHandler setRobot:nil];
+#endif
 }
 
-- (IBAction)zeroPressed:(id)sender {
+- (IBAction)zeroPressed:(id)sender
+{
+#if !TARGET_OS_SIMULATOR
     [_robot driveWithHeading:0.0 andVelocity:0.5];
+#endif
 }
 
-- (IBAction)ninetyPressed:(id)sender {
+- (IBAction)ninetyPressed:(id)sender
+{
+#if !TARGET_OS_SIMULATOR
     [_robot driveWithHeading:90 andVelocity:0.5];
+#endif
 }
 
-- (IBAction)oneEightyPressed:(id)sender {
+- (IBAction)oneEightyPressed:(id)sender
+{
+#if !TARGET_OS_SIMULATOR
     [_robot driveWithHeading:180 andVelocity:0.5];
+#endif
 }
 
-- (IBAction)twoSeventyPressed:(id)sender {
+- (IBAction)twoSeventyPressed:(id)sender
+{
+#if !TARGET_OS_SIMULATOR
     [_robot driveWithHeading:270.0 andVelocity:0.5];
+#endif
 }
 
-- (IBAction)stopPressed:(id)sender {
+- (IBAction)stopPressed:(id)sender
+{
+#if !TARGET_OS_SIMULATOR
     [_robot stop];
+#endif
 }
 
 @end
